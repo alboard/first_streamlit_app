846e71a7686918be3cbffccf41875504ec6356fe
alboard <alistair.board@gmail.com>
2022-09-09 15:38:06 +0100

    Let's Call the Fruityvice API from Our Streamlit App - get json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ streamlit.dataframe(fruits_to_show)
 
 # Let's Call the Fruityvice API from Our Streamlit App!
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
