4a482698b0ff491130ab717737e84d038a9757d3
alboard <alistair.board@gmail.com>
2022-09-09 15:44:19 +0100

    Interactive fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,9 @@ streamlit.dataframe(fruits_to_show)
 
 # Let's Call the Fruityvice API from Our Streamlit App!
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 # automap JSON into tabular dataframe
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
